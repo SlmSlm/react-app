fdf45a4bdb1c2e25a7d26b7abe8638ecebc77008
mac <mac@macs-MacBook-Pro.local>
2021-07-03 19:17:29 +0300

    Observer add

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import reportWebVitals from "./reportWebVitals";
-import { rerenderEntireTree } from "./render";
-import state from "./redux/state";
+import state, { subscribe, addPost, updateNewPostText } from "./redux/state";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App
+        state={state}
+        addPost={addPost}
+        updateNewPostText={updateNewPostText}
+      />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+};
 
 rerenderEntireTree(state);
+subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
