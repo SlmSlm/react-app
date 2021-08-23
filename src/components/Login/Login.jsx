3b1fc65befe8f2b5322fc2e86e3669c0d9b06b9d
mac <mac@macs-MacBook-Pro.local>
2021-08-23 03:57:43 +0300

    login/logout

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,21 +1,25 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Field, reduxForm } from "redux-form";
 import { required } from "../../utils/validators/validators";
 import { Input } from "../Common/FormsControls/FormsControls";
+import { login } from "../../redux/auth-reducer";
+import { Redirect } from "react-router-dom";
 
 const LoginForm = (props) => {
   return (
     <form onSubmit={props.handleSubmit}>
       <div>
         <Field
-          placeholder={"Login"}
-          name={"name"}
+          placeholder={"Email"}
+          name={"email"}
           component={Input}
           validate={[required]}
         />
       </div>
       <div>
         <Field
+          type={"password"}
           placeholder={"Password"}
           name={"password"}
           component={Input}
@@ -37,8 +41,12 @@ const LoginReduxForm = reduxForm({ form: "login" })(LoginForm);
 
 const Login = (props) => {
   const onSubmit = (formData) => {
-    console.log(formData);
+    props.login(formData.email, formData.password, formData.rememberMe);
   };
+
+  if (props.isAuth) {
+    return <Redirect to={"/profile"} />;
+  }
   return (
     <div>
       <h1>LOGIN</h1>
@@ -46,4 +54,9 @@ const Login = (props) => {
     </div>
   );
 };
-export default Login;
+
+const mapStateToProps = (state) => ({
+  isAuth: state.auth.isAuth,
+});
+
+export default connect(mapStateToProps, { login })(Login);
